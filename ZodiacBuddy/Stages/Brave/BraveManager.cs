ce8e4b5554b229984676e4c44f761b900b3a5e9b
Hiroa <hirroa@gmail.com>
2022-09-14 18:05:57 +0200

    feat(brave): Add brave toast detection

--- ZodiacBuddy/Stages/Brave/BraveManager.cs
+++ ZodiacBuddy/Stages/Brave/BraveManager.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Linq;
 
 using Dalamud.Game;
 using Dalamud.Game.Gui.Toast;
@@ -16,15 +17,17 @@ namespace ZodiacBuddy.Stages.Brave;
 /// </summary>
 internal class BraveManager : IDisposable
 {
+    // Any reason to not multiply all light level * 2 and put the progress on 80 instead of 40 ?
     private static readonly BonusLightLevel[] BonusLightValues =
     {
         #pragma warning disable format,SA1008,SA1025
-        new(  4, 4660), // Feeble
-        new(  8, 4661), // Faint
-        new( 16, 4662), // Gentle
-        new( 24, 4663), // Steady
-        new( 48, 4664), // Forceful
-        new( 64, 4665), // Nigh Sings
+        new( 4, 4660), // Feeble
+        new( 8, 4661), // Faint
+        new(16, 4662), // Gentle
+        new(24, 4663), // Steady
+        new(48, 4664), // Forceful
+        new(64, 4665), // Nigh Sings
+        new(40, 4666), // Completed
         #pragma warning restore format,SA1008,SA1025
     };
 
@@ -40,6 +43,7 @@ internal class BraveManager : IDisposable
         this.window = new BraveWindow();
 
         Service.Framework.Update += this.OnUpdate;
+        Service.Toasts.QuestToast += this.OnToast;
         Service.Interface.UiBuilder.Draw += this.window.Draw;
 
         SignatureHelper.Initialise(this);
@@ -56,8 +60,18 @@ internal class BraveManager : IDisposable
     public void Dispose()
     {
         Service.Interface.UiBuilder.Draw -= this.window.Draw;
+        Service.Toasts.QuestToast -= this.OnToast;
 
         this.addonRelicMagiciteOnSetupHook?.Disable();
+    }
+
+    private static bool CheckMessage(SeString toast, BonusLightLevel lightLevel)
+    {
+        var toastAsString = toast.ToString();
+        return lightLevel.Message.Split("  ")
+                   .All(text => toastAsString.Contains(text)) ||
+               lightLevel.Message.Split("「」") // Japanese
+                   .All(text => toastAsString.Contains(text));
     }
 
     private void AddonRelicMagiciteOnSetupDetour(IntPtr addonRelicMagicite, uint a2, IntPtr relicInfoPtr)
@@ -67,7 +81,6 @@ internal class BraveManager : IDisposable
         try
         {
             this.UpdateRelicMagiciteAddon(0);
-            // this.UpdateRelicMagiciteAddon(1, ???); // TODO component ID
         }
         catch (Exception ex)
         {
@@ -85,14 +98,6 @@ internal class BraveManager : IDisposable
         if (addon == null)
             return;
 
-        // var componentNode = (AtkComponentNode*)addon->UldManager.SearchNodeById(nodeID);
-        // if (componentNode == null)
-        //     return;
-
-        // var lightText = (AtkTextNode*)componentNode->Component->UldManager.SearchNodeById(9);
-        // if (lightText == null)
-        //     return;
-
         var lightText = (AtkTextNode*)addon->UldManager.SearchNodeById(9);
         if (lightText == null)
             return;
@@ -105,10 +110,6 @@ internal class BraveManager : IDisposable
 
         if (!Configuration.DontPlayRelicMagiciteAnimation)
             return;
-
-        // var analyzeText = (AtkTextNode*)componentNode->Component->UldManager.SearchNodeById(8);
-        // if (analyzeText == null)
-        //     return;
 
         var analyzeText = (AtkTextNode*)addon->UldManager.SearchNodeById(8);
         if (analyzeText == null)
@@ -137,15 +138,55 @@ internal class BraveManager : IDisposable
             return;
         }
 
+        // Brave sword and shield are managed together
+        // You can't take/validate magicite without both in your inventory
+        // and only the sword gain spiritbond with duty
         var mainhand = Util.GetEquippedItem(0);
-        var offhand = Util.GetEquippedItem(1);
 
-        var shouldShowWindow =
-            BraveRelic.Items.ContainsKey(mainhand.ItemID) ||
-            BraveRelic.Items.ContainsKey(offhand.ItemID);
+        var shouldShowWindow = BraveRelic.Items.ContainsKey(mainhand.ItemID);
 
         this.window.ShowWindow = shouldShowWindow;
         this.window.MainhandItem = mainhand;
-        this.window.OffhandItem = offhand;
+    }
+
+    private void OnToast(ref SeString message, ref QuestToastOptions options, ref bool isHandled)
+    {
+        try
+        {
+            this.OnToastInner(ref message, ref options, ref isHandled);
+        }
+        catch (Exception ex)
+        {
+            PluginLog.Error(ex, $"Unhandled error during {nameof(BraveManager)}.{nameof(this.OnToast)}");
+        }
+    }
+
+    private void OnToastInner(ref SeString message, ref QuestToastOptions options, ref bool isHandled)
+    {
+        if (isHandled)
+            return;
+
+        foreach (var lightLevel in BonusLightValues)
+        {
+            if (!CheckMessage(message, lightLevel))
+                continue;
+
+            if (lightLevel == BonusLightValues[6]) // Ignore completed magicite for now
+                return;
+
+            Service.Plugin.PrintMessage($"Light Intensity has increased by {lightLevel.Intensity}.");
+
+            var territoryId = Service.ClientState.TerritoryType;
+            if (!BonusLightDuty.TryGetValue(territoryId, out var territoryLight))
+                return;
+
+            // Brave light gain look like territoryLight.DefaultLightIntensity /2
+            // Still not sure about that, will need more data
+            if (lightLevel.Intensity > territoryLight!.DefaultLightIntensity / 2)
+            {
+                // Service.BonusLightManager.AddLightBonus(territoryId,
+                //     $"Light bonus detected on \"{territoryLight.DutyName}\"");
+            }
+        }
     }
 }
